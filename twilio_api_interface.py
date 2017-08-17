c1b56e8c2a346dbf48e2b5308aea87345271af55
Ammar Babar <ammarbabar@Ammars-MacBook-Pro.local>
2017-08-17 14:46:15 -0700

    commented the back_end app

--- twilio_api_interface.py
+++ twilio_api_interface.py
@@ -6,45 +6,98 @@ from requests.auth import HTTPBasicAuth
 import requests
 
 
-
 app = Flask(__name__)
 our_number = "+12069813239"
 shopify_API_key = '050e6e4e42481d41214b5949f37d2da1'
 shopify_API_pass = '8b9a5636bcbe3898cc4433ec31fb38f1'
 
-#abstract method that when the url is called, this file will extract the information given by the shopify site
-#returns it in a dictionary format
+
 def json_to_dictionary(json_string):
+	'''
+	Convert the json file returned by the shopify orders api to a dictionary
+	
+	@type json_string: string
+	@param json_string: json_string containing order details
+	
+	@rtype: dict
+	@return: dictionary of order details, for example, id: ..., shipping_lines: ..., ...
+	'''
 	json_string_temp = json_string[9:-1] #removing the prefix '{"order":' and suffix '}' from the string.
 	dictionary = json.loads(json_string_temp)
 	return dictionary
 
 def get_line_items_dictionary(json_string):
+	'''
+	Convert the line_item details in json file returned by the shopify orders api to a dictionary
+	
+	@type json_string: string
+	@param json_string: json_string containing order details
+	
+	@rtype: dict
+	@return: dictionary of line item details, for example, name: ..., quantity: ..., ...
+	'''
 	temp_string = json_string.split('line_items":[',1)[1]
 	temp_string = temp_string.split('"shipping_lines"',1)[0][0:-2]
 	line_items_dictionary = json.loads(temp_string)
 	return line_items_dictionary
-#abstract method that takes the dictionary and makes requests to our database based the item. 
-#Returns artisans phone number	
-def get_artisan_phone_number(dictionary):
+
+
+def get_artisan_phone_number(product_id):
+	'''
+	Method that takes the product id and queries Adisa database for the artisan's phone number	
+	
+	@type product_id: int
+	@param product_id: number representing the product in the database
+	
+	@rtype: str
+	@return: Artisan's phone number
+	'''
 	pass
 
-#abstract method that calls the shopify api that returns the details on the purchased item
-#returns json file
+
 def get_shopify_order_details(id):
-	# base_url = "http://%s:%s@shopadisa.myshopify.com/admin/orders/%s.json"  % ('050e6e4e42481d41214b5949f37d2da1', '8b9a5636bcbe3898cc4433ec31fb38f1', id)
+	'''
+	Method that calls the shopify orders api and returns a string in json format
+	with the details of the purchased item
+
+	@type id: str
+	@param id: id of transaction
+	
+	@rtype: str
+	@return: string containing order information in json format
+	'''
 	base_url = "https://shopadisa.myshopify.com/admin/orders/%s.json"  % id
 	response = requests.get(base_url, auth=(shopify_API_key, shopify_API_pass))
 	return response.content
 
-#abstract method that updates the sale info in our database with String status (aka purchased, delivered, etc)
-#returns nothing
+
 def update_database(dictionary, status):
+	'''
+	Method that updates the sale info in our database with status (aka purchased, delivered, etc)
+
+	@type dictionary: dict
+	@param dictionary: dictionary containing order details
+	
+	@type status: str
+	@param status: the status of the order i.e. purchases, delivered, etc.
+	'''
 	pass
 
-#method that uses extracted information to message the artisan with the tracking number for confirmation
-#NOTE: ADD TIMESTAMP
+
 def send_artisan_confirmation(line_items_dictionary, dictionary, artisan_phone_number):
+	'''
+	Method that uses extracted information to message the artisan with the tracking number for confirmation
+
+	@type line_items_dictionary: dict
+	@param line_items_dictionary: dictionary containing line item details
+	
+	@type dictionary: dict
+	@param dictionary: dictionary containing order details
+
+	@type artisan_phone_number: str
+	@param status: the phone number of the artisan to be texted
+	'''
+
 	#These keys are hard coded from the twilio site, they give us access to our purchased number
 	account_sid = "AC5c28e8e1e47b705b518337506b1f4fe0"
 	auth_token = "623d65e8232424274dac8ddd63a716d6"
@@ -65,30 +118,21 @@ def send_artisan_confirmation(line_items_dictionary, dictionary, artisan_phone_n
 	 	  "please call %s.Your payment will be processed shortly. Thank you, Adisa LLC" %  our_number)
 		
 
-
-#main function that executes all the backend
-#first calls the shopify API with the id number that is passed through the url
-#upon the return of the JSON file containing the item info, that is converted to a dictionary
-#and then used to reference our database to get the artisan phone number and update the sale status.
-#Then the artisan is sent a text message based on the retrieved phone number.
 @app.route("/<id>", methods=["GET", "POST"])
 def main(id):
-		
+	'''
+	Main function that executes all the backend
+
+	@type id: str
+	@param id: id of the transaction
+	
+	'''
 	json_string = get_shopify_order_details(id)
-	dick = json_to_dictionary(json_string)
-	smoalerr_dick = get_line_items_dictionary(json_string)
+	dictionary = json_to_dictionary(json_string)
+	line_items_dictionary = get_line_items_dictionary(json_string)
 	#update_database(dictionary)
 	#artisan_phone_number = get_artisan_phone_number(dictionary)
-	send_artisan_confirmation(smoalerr_dick, dick,'+16123561005')
-	return("The deed is done.")
-
-
+	send_artisan_confirmation(line_items_dictionary, dictionary,'+16123561005')
 
 if __name__ =="__main__":
 	app.run(debug=True)
-
-
-
-
-
-
